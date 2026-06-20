851bf4210a5fb663a822acf83db25fd7d48d274f
Nana Yaw Appiah Damoah <nana.damoah12@gmail.com>
2026-06-20 12:31:46 -0700

    Add final countdown sketch

---
+++ Assignment1_DamoahNanaYawAppiah.ino
@@ -0,0 +1,35 @@
+int startValue = 5;
+int ledPin = 13;
+
+void flashLED (int times) {
+  while (times > 0) {
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    delay(200);
+    times = times - 1;
+  }    
+}
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+
+  Serial.println("=== Smart Countdown Starting ===");
+
+  int count = startValue;
+  while (count > 0) {
+    Serial.print("Counter is: ");
+    Serial.println(count);
+
+    flashLED(count);
+
+    delay(1000);
+    count = count - 1;
+  }
+  
+  Serial.println("=== Countdown Complete ===");
+}
+
+void loop() {}
+
